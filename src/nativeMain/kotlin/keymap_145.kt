c4dc007898f8f2d8aa2477f5706964cd4a412182
Napharcos <napharcos.mail@gmail.com>
2025-12-18 16:05:48 +0100

    Version 1.1.4.1
    mouse reset time to 500

--- src/nativeMain/kotlin/keymap_145.kt
+++ src/nativeMain/kotlin/keymap_145.kt
@@ -146,7 +146,7 @@ const val KEYMAP_CONTENT_145_175 = """
 <SwitchOperation Description="射击" EnableSwitch="SetUp" DisableSwitch="BreastPatting|ReturnSetUp|Boomf" Point_X="0.060937" Point_Y="0.519444" HideTips="1"/>
 <SwitchOperation Description="射击" EnableSwitch="Reload" DisableSwitch="BreastPatting|ReturnSetUp|SelectFireMode|Boomf" Point_X="0.060937" Point_Y="0.519444" HideTips="1"/>
 </KeyMappingEx>
-<KeyMappingEx ItemName="Move with Mouse" Point_X="0.661719" Point_Y="0.369444" Description="视角" Remark="" MiniVisiable="false" MiniDisable="false" HideTipsAlways="1" AutoActive="1" Raw="1" Type="RClick" Offset="0.450000" LOCK_AsciiCode="%lockMouseCode" AsciiCode="%lockMouseCode" MouseResetTime="1000" Sensi_X="1.000000" Sensi_Y="1.000000">
+<KeyMappingEx ItemName="Move with Mouse" Point_X="0.661719" Point_Y="0.369444" Description="视角" Remark="" MiniVisiable="false" MiniDisable="false" HideTipsAlways="1" AutoActive="1" Raw="1" Type="RClick" Offset="0.450000" LOCK_AsciiCode="%lockMouseCode" AsciiCode="%lockMouseCode" MouseResetTime="500" Sensi_X="1.000000" Sensi_Y="1.000000">
 <SwitchOperation Description="视角" EnableSwitch="SetUp" DisableSwitch="ReturnSetUp|BreastPatting|SelectFireMode|HangUp|YellowM|Hand|Boomf|Transformation|InSetUp|Diamond1"/>
 <SwitchOperation Description="视角" EnableSwitch="Reload" DisableSwitch="ReturnSetUp|BreastPatting|SelectFireMode|HangUp|YellowM|Hand|Boomf|Transformation|InSetUp|Diamond1"/>
 <SwitchOperation Description="视角" EnableSwitch="SteeringWheel" DisableSwitch="ReturnSetUp|BreastPatting|SelectFireMode|HangUp|YellowM|Hand|Boomf|Transformation|InSetUp|Diamond1"/>
@@ -320,7 +320,7 @@ const val KEYMAP_CONTENT_145_566 = """
 <SwitchOperation Description="射击" EnableSwitch="SetUp" DisableSwitch="BreastPatting|ReturnSetUp|Boomf" Point_X="0.060937" Point_Y="0.519444" HideTips="1"/>
 <SwitchOperation Description="射击" EnableSwitch="Reload" DisableSwitch="BreastPatting|ReturnSetUp|SelectFireMode|Boomf" Point_X="0.060937" Point_Y="0.519444" HideTips="1"/>
 </KeyMappingEx>
-<KeyMappingEx ItemName="Move with Mouse" Point_X="0.661719" Point_Y="0.369444" Description="视角" Remark="" MiniVisiable="false" MiniDisable="false" HideTipsAlways="1" AutoActive="1" Raw="1" Type="RClick" Offset="0.450000" LOCK_AsciiCode="%lockMouseCode" AsciiCode="%lockMouseCode" MouseResetTime="1000" Sensi_X="1.000000" Sensi_Y="1.000000">
+<KeyMappingEx ItemName="Move with Mouse" Point_X="0.661719" Point_Y="0.369444" Description="视角" Remark="" MiniVisiable="false" MiniDisable="false" HideTipsAlways="1" AutoActive="1" Raw="1" Type="RClick" Offset="0.450000" LOCK_AsciiCode="%lockMouseCode" AsciiCode="%lockMouseCode" MouseResetTime="500" Sensi_X="1.000000" Sensi_Y="1.000000">
 <SwitchOperation Description="视角" EnableSwitch="SetUp" DisableSwitch="ReturnSetUp|BreastPatting|SelectFireMode|HangUp|YellowM|Hand|Boomf|Transformation|InSetUp|Diamond1"/>
 <SwitchOperation Description="视角" EnableSwitch="Reload" DisableSwitch="ReturnSetUp|BreastPatting|SelectFireMode|HangUp|YellowM|Hand|Boomf|Transformation|InSetUp|Diamond1"/>
 <SwitchOperation Description="视角" EnableSwitch="SteeringWheel" DisableSwitch="ReturnSetUp|BreastPatting|SelectFireMode|HangUp|YellowM|Hand|Boomf|Transformation|InSetUp|Diamond1"/>
